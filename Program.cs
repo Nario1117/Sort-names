d50ef5ebee7b917c737eaa67fee5ce31e58a052f
Nario <forgacnario2@gmail.com>
2022-04-17 19:46:41 +0200

    Sort names working

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Title = "Pocet deti";
+Console.WriteLine("Kolik je dětí ve třídě");
+
+int pocetDeti = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Napiš jména/o");
+
+string[] deti = new string[pocetDeti];
+
+for (int i = 0; i < pocetDeti; i++)
+{
+     deti[i] = Console.ReadLine();
+}
+
+
+
+Console.WriteLine("Tady jsou všechny jména postupne abecedně");
+
+Array.Sort(deti);
+
+for (int i = 0; (i) < deti.Length; i++)
+{
+    Console.WriteLine(deti[i]);
+}
+
+Console.ReadKey();
